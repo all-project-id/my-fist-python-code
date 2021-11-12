f2e9a85852656cd67b4fa935c3d38ea3c897ac63
sotarmunthe <sotar.sotar2018@gmail.com>
2021-11-12 16:06:25 +0700

    sekuensial dan percabangan sequintal

---
+++ Flowchart.py
@@ -0,0 +1,27 @@
+"""
+Semua sintaksis dasar bahasa pemrograman terdiri dari:
+1.Sekuansial :langkah berurutan
+2.Percabangan:langkah melompat jika kondisi terpenuhi
+3.Perulangan:mengulang langkah sampai berkali-kali selama/kondisi terpenuhi
+"""
+# sekuensial
+
+print('Ibu berkata, "Pergi ke toko"')
+print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
+print('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
+print("Maka Budi berangkat ke toko")
+print("Dan mulai berbelanja")
+
+# Percabangan
+jumlah_botol_susu =0
+
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    print("Budi membeli satu botol susu")
+
+else:
+    print("Budi tidak jadi membeli satu botol susu")
+
+print("Budi pulang kerumah")
+print("Menyampaikan hasilnya kepada ibu")
+
